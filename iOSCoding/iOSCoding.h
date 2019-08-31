ade1bfef2862f82956bac4cd8d39cf960567cc54
Theo Paris <theoplaysmc@gmail.com>
2019-08-30 17:43:19 -0700

    Initial Commit

---
+++ iOSCoding/iOSCoding.h
@@ -0,0 +1,19 @@
+//
+//  iOSCoding.h
+//  iOSCoding
+//
+//  Created by Theo Paris on 8/30/19.
+//  Copyright © 2019 Creepinson. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for iOSCoding.
+FOUNDATION_EXPORT double iOSCodingVersionNumber;
+
+//! Project version string for iOSCoding.
+FOUNDATION_EXPORT const unsigned char iOSCodingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <iOSCoding/PublicHeader.h>
+
+
